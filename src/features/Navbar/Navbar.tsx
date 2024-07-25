4b9f4204d267d3ce1446a1279509ada55f287373
Pablo <pablito@me.com>
2024-07-25 22:04:58 +0100

    feat: started to implement navbar styling and added a separate colour sheet.

--- src/features/Navbar/Navbar.tsx
+++ src/features/Navbar/Navbar.tsx
@@ -1,24 +1,21 @@
-import { useThemeContext } from "../../hooks/useThemeContext.tsx";
+// import { useThemeContext } from "../../hooks/useThemeContext.tsx";
 import NavLeft from "./NavLeft.tsx";
 import NavRight from "./NavRight.tsx";
 
 export default function Navbar() {
-  const { theme, handleThemeChange } = useThemeContext();
+  // const { theme } = useThemeContext();
 
   // 👇🏻 TEMPORARY variable to mimic future functionality
 
   return (
     <nav>
       <div className="nav-container">
-        <button onClick={handleThemeChange}>
-          {theme === "light" ? "light" : theme === "dark" ? "dark" : "colour"}
-        </button>
         <NavLeft />
         <NavRight />
       </div>
-      <div className={`nav-border-bar-1 nav-border-bar-1-${theme}`}></div>
-      <div className={`nav-border-bar-2 nav-border-bar-2-${theme}`}></div>
-      <div className={`nav-border-bar-3 nav-border-bar-3-${theme}`}></div>
+      <div className={`nav-border-bar-1`}></div>
+      <div className={`nav-border-bar-2`}></div>
+      <div className={`nav-border-bar-3`}></div>
     </nav>
   );
 }
