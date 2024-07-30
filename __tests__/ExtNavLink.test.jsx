1cb056547d8deb7efb70ac89a493e8e6db5ad799
Pablo <pablito@me.com>
2024-07-30 19:52:35 +0100

    feat: stopped trying to make assertions about ext nav link being clicked.

--- __tests__/ExtNavLink.test.jsx
+++ __tests__/ExtNavLink.test.jsx
@@ -1,11 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 import React from "react";
-import { test, describe, expect, vi, beforeEach, afterEach } from "vitest";
+import { test, describe, expect, vi } from "vitest";
 import ExtNavLink from "../src/features/Navbar/ExtNavLink";
 import {
   render,
-  screen,
+  // screen,
   within,
 } from "../test-setup/mockedContextProviders/MockAllContext";
 // import user from "@testing-library/user-event";
@@ -36,17 +34,6 @@ vi.mock("react-router-dom", async () => {
 });
 
 describe("first project NavLink", () => {
-  let openSpy;
-
-  beforeEach(() => {
-    // Spy on window.open using globalThis
-    openSpy = vi.spyOn(globalThis, "open").mockImplementation(() => null);
-  });
-
-  afterEach(() => {
-    openSpy.mockRestore();
-  });
-
   test(`renders the GitHub ExtNavLink component`, () => {
     // Arrange
     const { container } = renderComponent();
@@ -56,7 +43,7 @@ describe("first project NavLink", () => {
     const githubLogoImage = within(container).getByRole("image", {
       name: /Github logo/i,
     });
-    screen.logTestingPlaygroundURL();
+    // screen.logTestingPlaygroundURL();
     // screen.debug();
 
     // Assert
@@ -79,100 +66,4 @@ describe("first project NavLink", () => {
     expect(githubLink).toHaveAttribute("target", "_blank");
     expect(githubLink).toHaveAttribute("rel", "noreferrer");
   });
-
-  // test(`hovering over the project NavLink button calls the setFocusProjectId function once`, async () => {
-  //   // Arrange
-  //   const { container } = renderComponent();
-  //   const lupoNavLink = within(container).getByRole("button", {
-  //     name: /LUPO/i,
-  //   });
-  //
-  //   // Act
-  //   await user.hover(lupoNavLink);
-  //
-  //   // Assert
-  //   expect(mockSetFocusProjectId).toHaveBeenCalledTimes(1);
-  //   expect(mockSetFocusAboutId).toHaveBeenCalledTimes(0);
-  // });
-  //
-  // test(`unhovering the project NavLink button calls the setFocusProjectId function once`, async () => {
-  //   // Arrange
-  //   const { container } = renderComponent();
-  //   const lupoNavLink = within(container).getByRole("button", {
-  //     name: /LUPO/i,
-  //   });
-  //
-  //   // Act
-  //   await user.unhover(lupoNavLink);
-  //
-  //   // Assert
-  //   expect(mockSetFocusProjectId).toHaveBeenCalledTimes(1);
-  //   expect(mockSetFocusAboutId).toHaveBeenCalledTimes(0);
-  // });
 });
-
-// describe("first about NavLink", () => {
-//   beforeEach(() => {
-//     vi.clearAllMocks(); // or vi.resetAllMocks();
-//   });
-//
-//   test(`renders the about NavLink component with a button of expected text, "${exampleProject.navName}"`, () => {
-//     // Arrange
-//     const { container } = renderComponent("about", exampleAbout);
-//     const spaceNavLink = within(container).getByRole("button", {
-//       name: /Space Explorer/i,
-//     });
-//     // screen.logTestingPlaygroundURL();
-//     // screen.debug();
-//
-//     // Assert
-//     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//     // @ts-expect-error
-//     expect(spaceNavLink).toBeInTheDocument();
-//   });
-//
-//   test(`clicking the about NavLink button calls the setFocusProjectId function once`, () => {
-//     // Arrange
-//     const { container } = renderComponent("about", exampleAbout);
-//     const spaceNavLink = within(container).getByRole("button", {
-//       name: /Space Explorer/i,
-//     });
-//
-//     // Act
-//     spaceNavLink.click();
-//
-//     // Assert
-//     expect(mockSetSelectedAbout).toHaveBeenCalledTimes(1);
-//     expect(mockSetSelectedProject).toHaveBeenCalledTimes(0);
-//   });
-//
-//   test(`hovering over the about NavLink button calls the setFocusProjectId function once`, async () => {
-//     // Arrange
-//     const { container } = renderComponent("about", exampleAbout);
-//     const spaceNavLink = within(container).getByRole("button", {
-//       name: /Space Explorer/i,
-//     });
-//
-//     // Act
-//     await user.hover(spaceNavLink);
-//
-//     // Assert
-//     expect(mockSetFocusAboutId).toHaveBeenCalledTimes(1);
-//     expect(mockSetFocusProjectId).toHaveBeenCalledTimes(0);
-//   });
-//
-//   test(`unhovering the about NavLink button calls the setFocusProjectId function once`, async () => {
-//     // Arrange
-//     const { container } = renderComponent("about", exampleAbout);
-//     const spaceNavLink = within(container).getByRole("button", {
-//       name: /Space Explorer/i,
-//     });
-//
-//     // Act
-//     await user.unhover(spaceNavLink);
-//
-//     // Assert
-//     expect(mockSetFocusAboutId).toHaveBeenCalledTimes(1);
-//     expect(mockSetFocusProjectId).toHaveBeenCalledTimes(0);
-//   });
-// });
