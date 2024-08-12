d79ee56cea93b021ae661679bf28fc460966c58b
Pablo <pablito@me.com>
2024-08-12 19:20:34 +0100

    feat: first step to adding stretch matrix from test repo.

---
+++ src/features/StretchMatrix/StretchMatrix.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import "./StretchMatrix.css";
+
+export default function StretchMatrix() {
+  const [hoveredRow, setHoveredRow] = useState<number | null>(null);
+
+  const handleMouseEnterRow = (index: number) => {
+    setHoveredRow(index);
+  };
+
+  const handleMouseExitRow = () => {
+    setHoveredRow(null);
+  };
+
+  return (
+    <div className="rows-container">
+      {[...Array(3).keys()].map((index) => (
+        <MatrixRow
+          key={`row-${index}`}
+          // className={`row`}
+          rowIndex={index}
+          hoveredRow={hoveredRow}
+          handleMouseEnterRow={handleMouseEnterRow}
+          handleMouseExitRow={handleMouseExitRow}
+        />
+      ))}
+    </div>
+  );
+}
+
+interface MatrixRowProps {
+  rowIndex: number;
+  hoveredRow: number | null;
+  handleMouseEnterRow: (index: number) => void;
+  handleMouseExitRow: () => void;
+}
+
+function MatrixRow({
+  rowIndex,
+  hoveredRow,
+  handleMouseEnterRow,
+  handleMouseExitRow,
+}: MatrixRowProps) {
+  const [hoveredColumn, setHoveredColumn] = useState<number | null>(null);
+
+  const handleMouseEnterCol = (index: number) => {
+    setHoveredColumn(index);
+  };
+
+  const handleMouseExitCol = () => {
+    setHoveredColumn(null);
+  };
+
+  useEffect(() => {
+    console.log(`focus - col: ${hoveredColumn}, row: ${hoveredRow}`);
+  });
+
+  return (
+    <div
+      className={`single-row-container`}
+      onMouseEnter={() => handleMouseEnterRow(rowIndex)}
+      onMouseLeave={handleMouseExitRow}
+    >
+      {[...Array(4).keys()].map((index) => (
+        <MatrixPanel
+          key={index}
+          rowIndex={rowIndex}
+          colIndex={index}
+          hoveredRow={hoveredRow}
+          hoveredColumn={hoveredColumn}
+          onMouseEnterCol={handleMouseEnterCol}
+          onMouseExitCol={handleMouseExitCol}
+        />
+      ))}
+    </div>
+  );
+}
+
+interface MatrixPanelProps {
+  rowIndex: number;
+  colIndex: number;
+  hoveredRow: number | null;
+  onMouseEnterCol: (index: number) => void;
+  onMouseExitCol: () => void;
+  hoveredColumn: number | null;
+}
+
+function MatrixPanel({
+  rowIndex,
+  colIndex,
+  hoveredRow,
+  onMouseEnterCol,
+  onMouseExitCol,
+  hoveredColumn,
+}: MatrixPanelProps) {
+  const isHovered = hoveredColumn === colIndex;
+
+  return (
+    <div
+      className={`panel ${isHovered ? "active" : hoveredColumn === null ? "passive" : ""} ${hoveredRow === rowIndex ? "active-row" : hoveredRow === null ? "passive-row" : ""}`}
+      onMouseEnter={() => onMouseEnterCol(colIndex)}
+      onMouseLeave={onMouseExitCol}
+    >
+      <p className="text">{`Column ${colIndex}`}</p>
+      <p className="text">{`Row ${rowIndex}`}</p>
+    </div>
+  );
+}
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,8 +1,10 @@
 // import React from 'react';
 import "./SectionPage.css";
+import StretchMatrix from "../features/StretchMatrix/StretchMatrix.tsx";
 
 function ProjectsPage() {
-  return <div className="projects-container"></div>;
+  // return <div className="projects-container"></div>;
+  return <StretchMatrix />;
 }
 
 export default ProjectsPage;
